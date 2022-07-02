006b17797692bc7da55eab5e81bffa18320d9e78
kalipy <3069087972@qq.com>
2022-07-02 20:48:38 +0800

    学习笔记

---
+++ run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#./cpolar http 9999 &
+#
+#echo "cpolar pid: $!"
+#
+#reptyr $! > 1.log &
+#
+#echo "reptyr pid: $!"
+
+node watch.js
+
+
+#!/bin/bash
+ 
+#a=`./a.out`
+#echo "获取a.out的输出结果到变量a：$a"
+#./a.out &
+#echo "获取a.out进程的pid：$!"
+
+#./a.out
+#echo "1"
+#echo "a$!"
+#java T
+#echo "2"
+#echo "b$!"
+# 正常情况是同步执行
+#c
+#1
+#a
+#java
+#2
+#b
+
+#./a.out &
+#echo "1"
+#echo "a$!"
+#java T &
+#echo "2"
+#echo "b$!"
+# &是后台运行 异步执行
+#1
+#a11641
+#2
+#b11642
+#c
+#java
+
+#./a.out &
+#wait
+#echo "1"
+#echo "a$!"
+#java T &
+#wait
+#echo "2"
+#echo "b$!"
+# wait等待紫禁城结束 同步执行
+#c
+#1
+#a12852
+#java
+#2
+#b12853
+
